b7df2afc23272bb9c0847e04c9e07656093cd9ab
Alexander Mot <alexander@amot.dev>
2025-09-16 00:06:45 -0700

    Fix cascade deletes

--- app/models.py
+++ app/models.py
@@ -35,7 +35,7 @@ class PavedRating(Base):
     twistyness = Column(Integer, doc="Tightness and frequency of turns")
     intensity = Column(Integer, doc="Overall riding energy the road draws out, from mellow to adrenaline-pumping")
 
-    twist_id = Column(Integer, ForeignKey("twists.id"))
+    twist_id = Column(Integer, ForeignKey("twists.id", ondelete="CASCADE"))
     twist = relationship("Twist", back_populates="paved_ratings")
 
     def __repr__(self):
@@ -53,7 +53,7 @@ class UnpavedRating(Base):
     technicality = Column(Integer, doc="Challenge level from terrain features like rocks, ruts, sand, or mud")
     flow = Column(Integer, doc="Smoothness of the trail without constant disruptions or awkward sections")
 
-    twist_id = Column(Integer, ForeignKey("twists.id"))
+    twist_id = Column(Integer, ForeignKey("twists.id", ondelete="CASCADE"))
     twist = relationship("Twist", back_populates="unpaved_ratings")
 
     def __repr__(self):
